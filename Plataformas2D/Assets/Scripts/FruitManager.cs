63ad069b61d791157d15dce08471638539907424
Yohel <perezyohel@gmail.com>
2020-07-19 23:33:36 -0500

    anexo de texto al final de un nivel completado

--- Plataformas2D/Assets/Scripts/FruitManager.cs
+++ Plataformas2D/Assets/Scripts/FruitManager.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //para el cambio de escena
+using UnityEngine.UI; //para el uso del texto al finalizar un nivel
 
 public class FruitManager : MonoBehaviour
 {
-    //cuenta el numero de frutas que hay y comprueba que solo quede una
-   public void AllFruitsCollected()
+    public Text levelCleared; //texto de All fruits collected
+
+    private void Update()
     {
-        if (transform.childCount==1)
+        AllFruitsCollected(); //llama la funcion para verificar en cada frame cuantas frutas quedan
+    }
+
+    //cuenta el numero de frutas que hay y comprueba que solo quede una
+    public void AllFruitsCollected()
+    {
+        if (transform.childCount==0)
         {
             Debug.Log("No fruits left");
+            levelCleared.gameObject.SetActive(true);
+            Invoke("ChangeScene", 1); //invoca al metodo para cambiar de escena despues de 1 segundo
         }
+    }
+
+    //metodo para cambiar de escena
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //carga la escena siguiente 
     }
 }
