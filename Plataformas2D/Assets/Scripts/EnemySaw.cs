7e39a112df1d38f764ba10a5f1da604cdd5a99d2
Yohel <perezyohel@gmail.com>
2020-07-19 19:02:39 -0500

    diseño de los tres niveles

---
+++ Plataformas2D/Assets/Scripts/EnemySaw.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySaw : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Player Damaged");
+            Destroy(collision.gameObject);
+        }
+    }
+}
